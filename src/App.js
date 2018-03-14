8300adcbe61bfafe8a6d2ebd2efb8e9f889dcb28
Adam Figgat <afiggat@gmail.com>
2018-03-13 18:42:53 -0600

    first commit for my online portfolio

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Navbar from './components/navbar/Navbar';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
